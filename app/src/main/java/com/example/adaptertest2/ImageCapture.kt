dda2bc82022a73c027d808316ad7475411ed3b6d
pandesalNaSunog <floresjem8@gmail.com>
2022-04-08 11:27:50 +0800

    latest

--- app/src/main/java/com/example/adaptertest2/ImageCapture.kt
+++ app/src/main/java/com/example/adaptertest2/ImageCapture.kt
@@ -1,6 +1,7 @@
 package com.example.adaptertest2
 
 import android.Manifest
+import android.app.Activity
 import android.app.AlertDialog
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -12,6 +13,7 @@ import android.util.Base64
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
@@ -39,85 +41,87 @@ class ImageCapture : AppCompatActivity() {
             token = db.getToken()
             orderId = intent.getIntExtra("order_id", 0)
             val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            startActivityForResult(intent, 100)
+            startActivityForResult(intent,100, null)
         }
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if(requestCode == 100) {
+        Log.e("ImageCapture", resultCode.toString())
 
-
-            val uri = data?.data
-
-            try {
-                val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
-                val stream = ByteArrayOutputStream()
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 50, stream)
-                val bytes: ByteArray = stream.toByteArray()
-
-                val image = Base64.encodeToString(bytes, Base64.DEFAULT)
-
-                val progressBar = ProgressBar()
-                val progress = progressBar.showProgressBar(
-                    this,
-                    R.layout.loading,
-                    "Marking...",
-                    R.id.progressText
-                )
-                val alerts = RequestAlerts(this)
-
-                val jsonObject = JSONObject()
-                jsonObject.put("order_id", orderId)
-                jsonObject.put("image", image)
-
-                val request = jsonObject.toString()
-                    .toRequestBody("application/json".toMediaTypeOrNull())
-                CoroutineScope(Dispatchers.IO).launch {
-                    val markAsCompleteResponse = try {
-                        RetrofitInstance.retro.markOrderAsCompleted(
-                            "Bearer $token",
-                            request
-                        )
-                    } catch (e: SocketTimeoutException) {
-                        withContext(Dispatchers.Main) {
-                            progress.dismiss()
-                            alerts.showSocketTimeOutAlert()
-                        }
-                        return@launch
-                    } catch (e: Exception) {
-                        withContext(Dispatchers.Main) {
-                            progress.dismiss()
-                            alerts.noInternetAlert()
-                        }
-                        return@launch
-                    }
-
-                    withContext(Dispatchers.Main) {
-                        progress.dismiss()
-                        if (markAsCompleteResponse.code() == 200 && markAsCompleteResponse.headers()
-                                .contains(Pair("content-type", "application/json"))
-                        ) {
-                            Toast.makeText(
-                                this@ImageCapture,
-                                "Proof of transaction has been uploaded",
-                                Toast.LENGTH_LONG
-                            ).show()
-                            val intent = Intent(this@ImageCapture, Navigation::class.java)
-                            startActivity(intent)
-                            finishAffinity()
-                        } else {
-                            AlertDialog.Builder(this@ImageCapture)
-                                .setTitle("Error")
-                                .setMessage("Something went wrong.")
-                                .setPositiveButton("OK", null)
-                                .show()
-                        }
-                    }
-                }
-            } catch (e: Exception) {
-                Log.e("ImageCapture", e.toString())
-            }
-        }
+//        if(requestCode == 100 && resultCode == Activity.RESULT_OK) {
+//
+//
+//            val uri = data?.data
+//            Log.e("ImageCapture", uri.toString())
+//            try {
+//                val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
+//                val stream = ByteArrayOutputStream()
+//                bitmap.compress(Bitmap.CompressFormat.JPEG, 50, stream)
+//                val bytes: ByteArray = stream.toByteArray()
+//
+//                val image = Base64.encodeToString(bytes, Base64.DEFAULT)
+//
+//                val progressBar = ProgressBar()
+//                val progress = progressBar.showProgressBar(
+//                    this,
+//                    R.layout.loading,
+//                    "Marking...",
+//                    R.id.progressText
+//                )
+//                val alerts = RequestAlerts(this)
+//
+//                val jsonObject = JSONObject()
+//                jsonObject.put("order_id", orderId)
+//                jsonObject.put("image", image)
+//
+//                val request = jsonObject.toString()
+//                    .toRequestBody("application/json".toMediaTypeOrNull())
+//                CoroutineScope(Dispatchers.IO).launch {
+//                    val markAsCompleteResponse = try {
+//                        RetrofitInstance.retro.markOrderAsCompleted(
+//                            "Bearer $token",
+//                            request
+//                        )
+//                    } catch (e: SocketTimeoutException) {
+//                        withContext(Dispatchers.Main) {
+//                            progress.dismiss()
+//                            alerts.showSocketTimeOutAlert()
+//                        }
+//                        return@launch
+//                    } catch (e: Exception) {
+//                        withContext(Dispatchers.Main) {
+//                            progress.dismiss()
+//                            alerts.noInternetAlert()
+//                        }
+//                        return@launch
+//                    }
+//
+//                    withContext(Dispatchers.Main) {
+//                        progress.dismiss()
+//                        if (markAsCompleteResponse.code() == 200 && markAsCompleteResponse.headers()
+//                                .contains(Pair("content-type", "application/json"))
+//                        ) {
+//                            Toast.makeText(
+//                                this@ImageCapture,
+//                                "Proof of transaction has been uploaded",
+//                                Toast.LENGTH_LONG
+//                            ).show()
+//                            val intent = Intent(this@ImageCapture, Navigation::class.java)
+//                            startActivity(intent)
+//                            finishAffinity()
+//                        } else {
+//                            AlertDialog.Builder(this@ImageCapture)
+//                                .setTitle("Error")
+//                                .setMessage("Something went wrong.")
+//                                .setPositiveButton("OK", null)
+//                                .show()
+//                        }
+//                    }
+//                }
+//            } catch (e: Exception) {
+//                Log.e("ImageCapture", e.toString())
+//            }
+//        }
     }
 }
